e6a408e68c60d9bfb9bffed169b8476b25d795ea
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-01-23 12:59:57 -0800

    Added Stepper for Value Attribute on ItemCreate Page

--- Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
@@ -50,5 +50,10 @@ namespace Mine.Views
         {
             await Navigation.PopModalAsync();
         }
+
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
