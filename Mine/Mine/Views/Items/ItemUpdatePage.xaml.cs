5a6b6e80d72141b15834ec95c90028232179ecb5
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-01-29 16:21:54 -0800

    Changed ItemUpdate to be a Modal Page

--- Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -36,8 +36,8 @@ namespace Mine.Views
         async void Save_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Update", ViewModel.Data);
-            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
-            await Navigation.PopAsync();
+            //Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
+            await Navigation.PopModalAsync();
         }
 
         /// <summary>
@@ -47,14 +47,19 @@ namespace Mine.Views
         /// <param name="e"></param>
         async void Cancel_Clicked(object sender, EventArgs e)
         {
-            Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
+            //Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
 
-            await Navigation.PopAsync();
+            await Navigation.PopModalAsync();
         }
 
         void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
         {
             ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
         }
     }
 }
